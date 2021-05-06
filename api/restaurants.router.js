796bc5f98120ba81c2d732c9858d08bc1cf49215
hafifamudi <hafifcyber@gmail.com>
2021-05-06 21:13:36 +0800

    add: restaurants controller and router

--- api/restaurants.router.js
+++ api/restaurants.router.js
@@ -1,10 +1,8 @@
 import express from 'express'
+import RestaurantsController from './restaurants.controller.js'
 
 const router = express.Router()
 
-router.route("/")
-    .get((req, res) => {
-        res.send("Hello, World")
-    })
+router.route("/").get(RestaurantsController.apiGetRestaurants)
 
 export default router
